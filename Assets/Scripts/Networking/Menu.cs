9d38c1571b137fca27fc772a5cc2b830b8fb37e4
KHowlett <howlettkieran@gmail.com>
2020-10-12 18:44:02 +0100

    Network connection established and RNG state shared.

---
+++ Assets/Scripts/Networking/Menu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+	[SerializeField] Button lightButton;
+	[SerializeField] Button darkButton;
+
+	public void StartLight()
+	{
+		lightButton.interactable = false;
+		darkButton.interactable = true;
+		NetworkManager mngr = NetworkManager.GetInstance();
+		mngr.Shutdown();
+		mngr.StartServer();
+	}
+
+	public void StartDark()
+	{
+		lightButton.interactable = true;
+		darkButton.interactable = false;
+		NetworkManager mngr = NetworkManager.GetInstance();
+		mngr.Shutdown();
+		mngr.StartClient();
+	}
+}
