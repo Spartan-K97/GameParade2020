4a5df686bc63be764c21d671522db4e72b9fca54
KHowlett <howlettkieran@gmail.com>
2020-10-13 12:13:20 +0100

    Removed networking code after game design change

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+	public void StartLight()
+	{
+		SceneManager.LoadScene("Scenes/PlayAsLight");
+	}
+
+	public void StartDark()
+	{
+		SceneManager.LoadScene("Scenes/PlayAsDark");
+	}
+}
