366e01bef6bc3d254f7c13c46faa7bf526ab487d
Tacey127 <jtacey@hotmail.co.uk>
2020-10-13 13:53:51 +0100

    Player can pick up orb and exit

---
+++ Assets/Scripts/Interactable/InteractableOrb.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableOrb : Interactable
+{
+    [SerializeField] GameObject exit;
+    public override void Interact(PlayerInteraction interact)
+    {
+        if (interact.isRunner)
+        {
+            exit.SetActive(true);
+
+            Destroy(gameObject);
+        }
+    }
+}
